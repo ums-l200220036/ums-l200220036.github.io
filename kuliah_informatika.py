fcb0310acfbd2002dab520f6cc710d0f12a4cd17
ums-l200220036 <l200220036@student.ums.ac.id>
2025-01-04 11:28:19 +0700

    Pesan commit yang menggambarkan perubahan

--- kuliah_informatika.py
+++ kuliah_informatika.py
@@ -1,82 +1,44 @@
-class Mahasiswa:
-    def __init__(self, nama, nim):
-        self.nama = nama
-        self.nim = nim
-        self.spp_terbayar = False
-        self.kehadiran = 0
-        self.tugas = []
-        self.uts = None
-        self.uas = None
-        self.nilai_akhir = None
+from metaflow import FlowSpec, step
 
+class KuliahFlow(FlowSpec):
+    @step
+    def start(self):
+        print("Memulai proses mengikuti kuliah di program Informatika.")
+        self.next(self.bayar_spp)
+
+    @step
     def bayar_spp(self):
-        print(f"{self.nama} sedang membayar SPP...")
-        self.spp_terbayar = True
-        print("SPP telah dibayar.")
+        print("Mahasiswa melakukan pembayaran SPP untuk semester ini.")
+        self.next(self.registrasi_kuliah)
 
-    def ikuti_kuliah(self, jumlah_pertemuan):
-        if not self.spp_terbayar:
-            print("Harap bayar SPP terlebih dahulu!")
-            return
+    @step
+    def registrasi_kuliah(self):
+        print("Mahasiswa melakukan registrasi mata kuliah.")
+        self.next(self.mengikuti_kuliah)
 
-        for i in range(1, jumlah_pertemuan + 1):
-            print(f"{self.nama} menghadiri kuliah ke-{i}")
-            self.kehadiran += 1
+    @step
+    def mengikuti_kuliah(self):
+        print("Mahasiswa aktif mengikuti perkuliahan.")
+        self.next(self.mengerjakan_tugas)
 
-    def kumpul_tugas(self, nilai_tugas):
-        if not self.spp_terbayar:
-            print("Harap bayar SPP terlebih dahulu!")
-            return
+    @step
+    def mengerjakan_tugas(self):
+        print("Mahasiswa mengerjakan tugas-tugas dari dosen.")
+        self.next(self.mengerjakan_ujian)
 
-        print(f"{self.nama} mengumpulkan tugas dengan nilai {nilai_tugas}")
-        self.tugas.append(nilai_tugas)
+    @step
+    def mengerjakan_ujian(self):
+        print("Mahasiswa mengikuti ujian tengah semester dan ujian akhir semester.")
+        self.next(self.mendapatkan_nilai)
 
-    def ujian_uts(self, nilai):
-        if not self.spp_terbayar:
-            print("Harap bayar SPP terlebih dahulu!")
-            return
+    @step
+    def mendapatkan_nilai(self):
+        print("Mahasiswa mendapatkan nilai akhir mata kuliah.")
+        self.next(self.end)
 
-        print(f"{self.nama} mengikuti UTS dengan nilai {nilai}")
-        self.uts = nilai
+    @step
+    def end(self):
+        print("Proses perkuliahan selesai. Mahasiswa berhasil menyelesaikan semester.")
 
-    def ujian_uas(self, nilai):
-        if not self.spp_terbayar:
-            print("Harap bayar SPP terlebih dahulu!")
-            return
-
-        print(f"{self.nama} mengikuti UAS dengan nilai {nilai}")
-        self.uas = nilai
-
-    def hitung_nilai_akhir(self):
-        if not self.spp_terbayar:
-            print("Harap bayar SPP terlebih dahulu!")
-            return
-
-        if self.uts is None or self.uas is None or not self.tugas:
-            print("Nilai belum lengkap. Pastikan UTS, UAS, dan tugas sudah ada.")
-            return
-
-        rata_tugas = sum(self.tugas) / len(self.tugas)
-        self.nilai_akhir = 0.3 * rata_tugas + 0.3 * self.uts + 0.4 * self.uas
-        print(f"Nilai akhir {self.nama} adalah {self.nilai_akhir:.2f}")
-
-# Simulasi Proses Kuliah
-mahasiswa1 = Mahasiswa("Andi", "12345678")
-
-# Bayar SPP
-mahasiswa1.bayar_spp()
-
-# Mengikuti kuliah sebanyak 10 pertemuan
-mahasiswa1.ikuti_kuliah(10)
-
-# Mengumpulkan 3 tugas
-mahasiswa1.kumpul_tugas(80)
-mahasiswa1.kumpul_tugas(85)
-mahasiswa1.kumpul_tugas(90)
-
-# Mengikuti UTS dan UAS
-mahasiswa1.ujian_uts(88)
-mahasiswa1.ujian_uas(92)
-
-# Menghitung nilai akhir
-mahasiswa1.hitung_nilai_akhir()
+if __name__ == "__main__":
+    KuliahFlow()
